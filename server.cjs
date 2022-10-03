10337b6eeac105669b6877563b3387a4655e7f98
Mateus Louzada <mateuslouzada01@gmail.com>
2022-10-02 22:16:31 -0300

    Adicionando serverjs

--- server.cjs
+++ server.cjs
@@ -1,12 +1,7 @@
 const express = require("express");
-const path = require("path");
 const { resolve } = require("path");
-const { fileURLToPath } = require("url");
 
 const app = express();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 app.use("/", express.static(resolve(__dirname, "./dist")));
 
